05d243ef96652bec34c7cbbb10d38d4036501aef
fedehuguet <fede.huguet14@gmail.com>
2017-08-30 00:30:31 -0500

    forecaste var

--- intro.py
+++ intro.py
@@ -5,8 +5,11 @@ df = quandl.get('WIKI/GOOGL')
 
 df = df[['Adj. Open', 'Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 
-df['HL_PCT'] = (df['Adj. High']-df['Adj. Close'])/ df['Adj. Close'] #Margin of volatility
+df['HL_PCT'] = (df['Adj. High']-df['Adj. Close'])/ df['Adj. Close']*100 #Margin of volatility
 
-df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/ df['Adj. Open'] #Margin of old vs new
+df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/ df['Adj. Open']*100 #Margin of old vs new
 
 df = df[['Adj. Close', 'HL_PCT','PCT_change','Adj. Volume']] #Volume: How many sales were made
+
+forecaste_col = 'Adj. Close'
+df.fillna(-9999, implace=True)
