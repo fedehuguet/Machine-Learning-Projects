8375e308b0fb7a48a1881efc5db6808b695da54f
fedehuguet <fede.huguet14@gmail.com>
2017-09-01 15:43:29 -0500

    Selecting and organizing array

--- intro.py
+++ intro.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -11,5 +12,10 @@ df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/ df['Adj. Open']*100 #Marg
 
 df = df[['Adj. Close', 'HL_PCT','PCT_change','Adj. Volume']] #Volume: How many sales were made
 
-forecaste_col = 'Adj. Close'
-df.fillna(-9999, implace=True)
+forecast_col = 'Adj. Close'
+df.fillna(-9999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df))) #Try to predict out of 1% of the data, 10 days in the future aprox.
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.head())
