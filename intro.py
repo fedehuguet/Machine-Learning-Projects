3bce3d793281060ca85dc8b3990dfd2b53fc97f6
fedehuguet <fede.huguet14@gmail.com>
2017-09-10 14:47:49 -0500

    Graphing forecasforecast of next 30 days pricing

--- intro.py
+++ intro.py
@@ -1,9 +1,15 @@
+#Predicts next 30 days of prices
 import pandas as pd
 import quandl
 import math
+import datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -21,17 +27,43 @@ df.fillna(-9999, inplace=True)
 forecast_out = int(math.ceil(0.01*len(df))) #Try to predict out of 1% of the data, 10 days in the future aprox.
 print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 x = np.array(df.drop(['label'],1))
-y = np.array(['label'])
 x = preprocessing.scale(x)
+x_lately = x[-forecast_out:]
+x = x[:-forecast_out]
+
+df.dropna(inplace=True)
+y = np.array(['label'])
 y = np.array(df['label'])
 
 x_train, x_test, y_train, y_test = cross_validation.train_test_split(x,y,test_size=0.2)
 
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
 #clf = svm.SVR()
 clf.fit(x_train, y_train)
 accuracy = clf.score(x_test, y_test)
-print(accuracy)
+#print(accuracy)
+forecast_set = clf.predict(x_lately) #doing a prediction with a classifier is easy
+
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+#last_unix = last_date.timestamp()
+last_unix = (last_date - datetime.datetime(1970,1,1)).total_seconds()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_day
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
